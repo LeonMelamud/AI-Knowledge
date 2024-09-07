c9403a590c022ad33fa2ad2b62ff6a33378a8d07
Leon.Melamud <leon.melamud@flywire.com>
2024-09-07 21:21:38 +0300

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'), // תיקיית הפלט
+    },
+    mode: 'production', // מצב הבנייה (production/development)
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                        },
+                    },
+                    {
+                        loader: ImageMinimizerPlugin.loader,
+                        options: {
+                            severityError: 'warning', // Ignore errors on corrupted images
+                            minimizerOptions: {
+                                plugins: ['gifsicle', 'mozjpeg', 'optipng', 'svgo'],
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // קובץ ה-HTML שלך
+            filename: 'index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/css', to: 'css' },
+                { from: 'public/js', to: 'js' },
+                { from: 'public/data.json', to: 'data.json' }
+            ]
+        })
+    ],
+    optimization: {
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
+};
